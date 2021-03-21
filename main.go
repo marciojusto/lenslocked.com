f516730dd0932830ba786a6846196b4daaa5313c
Marcio Oliveira <marcio.oliveira@teamwillgroup.com>
2021-03-21 16:26:05 +0000

    Adding a 404 page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email do <a href=\"mailto:support@lenslocked.com\">support@lenlocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for... :-(</h1>")
 	}
 }
 
