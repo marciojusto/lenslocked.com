5c84a45f443d314f80efc75e7108f545d2ebcdf7
Marcio Oliveira <marcio.oliveira@teamwillgroup.com>
2021-03-21 14:22:50 +0000

    A simple router

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email do <a href=\"mailto:support@lenslocked.com\">support@lenlocked.com</a>.")
+	}
 }
 
 func main() {
